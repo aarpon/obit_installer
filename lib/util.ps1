de326c94e791e2add986d7aa26c9619ca34f37ba
Aaron Ponti <aarpon@bsse.ethz.ch>
2016-12-12 11:39:19 +0100

    Make Set-FullPermissionTo-* function recursive.

--- lib/util.ps1
+++ lib/util.ps1
@@ -18,56 +18,6 @@ function Save-Shortcut($sourceExe, $destinationPath)
     $Shortcut.Save()
 }
 
-# Set full permissions to Everyone.
-#
-# This function does not recurse since it is assumed that the folder is empty.
-function Set-FullPermissionToEveryone($folder)
-{
-    # Make sure $folder is a directory
-    if (! ((Get-Item $folder) -is [System.IO.DirectoryInfo]))
-    { 
-        Write-Host "$folder is not a valid directory."
-        Write-Host "Skipping"
-        return;
-    }
-
-    # Get the ACLs for $folder
-    $acl = Get-Acl $folder
-
-    # Access rule
-    $AccessRule= New-Object System.Security.AccessControl.FileSystemAccessRule("Everyone",
-        "FullControl", "ContainerInherit, Objectinherit", "None", "Allow")
-
-    # Set access sule
-    $acl.SetAccessRule($accessRule)
-    $acl | Set-Acl $folder
-}
-
-# Set full permissions to specific user.
-#
-# This function does not recurse since it is assumed that the folder is empty.
-function Set-FullPermissionToUser($folder, $username)
-{
-    # Make sure $folder is a directory
-    if (! ((Get-Item $folder) -is [System.IO.DirectoryInfo]))
-    { 
-        Write-Host "$folder is not a valid directory."
-        Write-Host "Skipping"
-        return;
-    }
-
-    # Get the ACLs for $folder
-    $acl = Get-Acl $folder
-
-    # Access rule
-    $AccessRule= New-Object System.Security.AccessControl.FileSystemAccessRule($username,
-        "FullControl", "ContainerInherit, Objectinherit", "None", "Allow")
-
-    # Set access sule
-    $acl.SetAccessRule($accessRule)
-    $acl | Set-Acl $folder
-}
-
 # Uninstall Datamover JSL as a Windows Service
 function Remove-DatamoverJSL($datamoverJSLPath, $platformNBits)
 {
